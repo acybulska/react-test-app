f9893177f1a1a14c9b2b9c4debc08eb6e9e197aa
Aleksandra Lisa Cybulska <aleksandralisacybulska@gmail.com>
2018-11-14 17:25:28 +0100

    Udemy Course starting commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Text from './components/Text';
-import ListResult from './components/ListResult';
+import Person from './Person/Person'
 
 class App extends Component {
-  state = {
-    subtitle: "to jest podtytul ze stanu",
-    showResults: false,
-    results: [
-      {
-        name: "Lisa",
-        age: "24"
-      },
-      {
-        name: "Wade",
-        age: "33"
-      },
-      {
-        name: "Tony",
-        age: "42"
-      }
-    ]
-  }
-
-  alertHandler = () => {
-    alert("TO JEST ALARM");
-  }
-
-  constructor(props) {
-    super(props);
-    this.toggleResultsHandler = this.toggleResultsHandler.bind(this);
-  }
-
-  toggleResultsHandler() {
-    this.setState({ showResults: !this.state.showResults })
-  }
   render() {
     return (
-      <div className="App">
-        <h2 style={{ borderBottom: 'red 1px solid' }}>Jest to jakis tytul</h2>
-        <Text color="blue" subtitle={this.state.subtitle}></Text>
-        <button onClick={this.toggleResultsHandler}>Pokaz wyniki</button>
-        {
-          this.state.showResults ? <ListResult alert={this.alertHandler} data={this.state.results}></ListResult> : null
-        }
+      <div>
+        <h1>Hello world</h1>
+        <Person></Person>
       </div>
     );
   }
