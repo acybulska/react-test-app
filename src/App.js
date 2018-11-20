881e2bf9d0e9021e1a4d9da406f667a73ed3d171
Aleksandra Lisa Cybulska <aleksandralisacybulska@gmail.com>
2018-11-20 13:56:33 +0100

    Working on styles

--- src/App.js
+++ src/App.js
@@ -5,23 +5,46 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      {name: "Max", age: 28},
-      {name: "Manu", age: 29},
-      {name: "Lisa", age: 24}
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Lisa", age: 24 }
     ]
   }
 
-  switchNameHandler = () => {
-    console.log("GO BUTTON");
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Lisa", age: 24 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: "Lisa", age: 24 }
+      ]
+    })
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div>
         <h1>Hello world</h1>
-        <button onClick={this.switchNameHandler}>Do nothing</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <button style={style} onClick={this.switchNameHandler.bind(this, "Ryder")}>Do nothing</button>
+        <Person click={() => this.switchNameHandler("Lydia")} name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
-    return <p>I'm {props.name} and I'm {props.age}</p>
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I'm {props.age}</p>
+            <input type="text" onChange={props.changed} />
+        </div>
+    )
 }
 
 export default person;
