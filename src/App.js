1f5a049390c9361362b7cddb1d39e0a04e60f829
Aleksandra Lisa Cybulska <aleksandralisacybulska@gmail.com>
2018-11-20 12:46:36 +0100

    Learning state and props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 29},
+      {name: "Lisa", age: 24}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("GO BUTTON");
+  }
+
   render() {
     return (
       <div>
         <h1>Hello world</h1>
-        <Person></Person>
+        <button onClick={this.switchNameHandler}>Do nothing</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
